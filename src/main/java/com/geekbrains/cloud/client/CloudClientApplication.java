0b30de93a21f5296478b09b1b14e25ee4719b597
Alex <ivchenko.aat@gmail.com>
2022-02-12 15:04:03 +0300

    dz

--- src/main/java/com/geekbrains/cloud/client/CloudClientApplication.java
+++ src/main/java/com/geekbrains/cloud/client/CloudClientApplication.java
@@ -6,14 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Objects;
-
 public class CloudClientApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent parent = FXMLLoader.load(
-                Objects.requireNonNull(getClass().getResource("layout.fxml"))
+                getClass().getResource("layout.fxml")
         );
         primaryStage.setScene(new Scene(parent));
         primaryStage.show();
